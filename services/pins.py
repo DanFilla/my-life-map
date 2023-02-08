08a22812966867cd36f71ea7682b4da6555d68c8
Dan Filla <danfilla@Dans-MacBook-Pro.local>
2023-02-08 17:51:01 -0600

    handling bad coords

--- services/pins.py
+++ services/pins.py
@@ -1,3 +1,4 @@
+from fastapi import HTTPException
 from sqlalchemy.orm import Session
 
 from schemas.pins import PinCreateSchema
@@ -10,6 +11,21 @@ class PinService:
 
     
     def create_pin(self, pin: PinCreateSchema, db: Session):
+
+        split_long = str(pin.longitude).split('.')
+        longitude_whole = split_long[0]
+        longitude_fractional = split_long[1]
+
+        split_lat= str(pin.latitude).split('.')
+        latitude_whole = split_lat[0]
+        latitude_fractional = split_lat[1]
+
+        if abs(pin.longitude) > 180 or abs(pin.latitude) > 180:
+            pin.longitude = float(longitude_whole + longitude_fractional[:6])
+            pin.latitude = float(latitude_whole + latitude_fractional[:6])
+
+            raise HTTPException(status_code=422, detail="improper format for coordinates")
+
         db_pin= Pin(longitude=pin.longitude, latitude=pin.latitude)
         db.add(db_pin)
         db.commit()
