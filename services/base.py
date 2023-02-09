38cfac4b54107d02afb8b7ff38cde2b4314593de
Dan Filla <danfilla@Dans-MacBook-Pro.local>
2023-02-09 11:06:41 -0600

    Added error catching for get service

--- services/base.py
+++ services/base.py
@@ -1,4 +1,5 @@
 import logging
+from fastapi import HTTPException
 from fastapi.encoders import jsonable_encoder
 from typing import TypeVar, Generic, Type
 from sqlalchemy.orm import Session
@@ -12,16 +13,20 @@ logger = logging.getLogger(__name__)
 CreateSchemaType = TypeVar("CreateSchemaType", bound=BaseCreateSchema)
 ModelType = TypeVar("ModelType", bound=Base)
 
+
 class BaseService(Generic[ModelType, CreateSchemaType]):
 
     def __init__(self, model: Type[ModelType]):
         self.model = model
 
     def get_one_by_id(self, id, db: Session):
-        return db.query(self.model).filter(self.model.id == id).first()
+        retrived_resource = db.query(self.model).filter(self.model.id == id).first()
+        if retrived_resource:
+            return retrived_resource
+        else:
+            raise HTTPException(status_code=404, detail="resource not found")
 
     def create_one(self, create_schema: CreateSchemaType, db: Session, *args, **kwargs):
-
         encoded_create_schema = jsonable_encoder(create_schema)
         create_object = self.model(**encoded_create_schema)
 
