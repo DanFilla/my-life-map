286f25cf10276a7aabb81bd114aea6a072ad9242
Dan Filla <danfilla@Dans-MacBook-Pro.local>
2023-02-08 18:57:53 -0600

    refactor pins service

--- services/pins.py
+++ services/pins.py
@@ -12,18 +12,7 @@ class PinService:
     
     def create_pin(self, pin: PinCreateSchema, db: Session):
 
-        split_long = str(pin.longitude).split('.')
-        longitude_whole = split_long[0]
-        longitude_fractional = split_long[1]
-
-        split_lat= str(pin.latitude).split('.')
-        latitude_whole = split_lat[0]
-        latitude_fractional = split_lat[1]
-
         if abs(pin.longitude) > 180 or abs(pin.latitude) > 180:
-            pin.longitude = float(longitude_whole + longitude_fractional[:6])
-            pin.latitude = float(latitude_whole + latitude_fractional[:6])
-
             raise HTTPException(status_code=422, detail="improper format for coordinates")
 
         db_pin= Pin(longitude=pin.longitude, latitude=pin.latitude)
