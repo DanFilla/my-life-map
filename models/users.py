9ec186136e2ea71bf06bc77353cd27e5257ebba3
Dan Filla <danfilla@gmail.com>
2023-02-17 11:46:03 -0600

    Added fields to user table

--- models/users.py
+++ models/users.py
@@ -6,6 +6,10 @@ from models.base import Base
 class UserModel(Base):
     __tablename__ = "users"
     
+    first_name = mapped_column(String(50))
+    last_name = mapped_column(String(50))
+    email = mapped_column(String(100), nullable=False, default="come@mebro.com")
+    user_name = mapped_column(String(50), nullable=False, default="default")
     description = mapped_column(String(256))
 
     #relationships
