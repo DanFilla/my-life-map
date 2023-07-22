372ad23fa514ed3fc41ba7b7b524d81cb39f4950
Dan Filla <danfilla@gmail.com>
2023-07-22 15:40:26 -0500

    add logging

--- tests/test_pins.py
+++ tests/test_pins.py
@@ -18,7 +18,7 @@ def test_create_pins():
 
         with PinManager(client=test_client, user_id=user_id) as pin:
             pin_body = pin.json()
-            logger.warning("pin_body: " + str(pin_body))
+            logger.warning("pin_id: " + str(pin_body.get(id)))
 
             assert type(pin_body.get("id")) == int
 
@@ -26,10 +26,12 @@ def test_create_pins():
 def test_read_pins():
     with UserManager(client=test_client) as user:
         user_id = user.json().get("id")
+        logger.warning("user_id: " + str(user_id))
 
         with PinManager(client=test_client, user_id=user_id) as pin:
             pin_body = pin.json()
             pin_id = pin_body.get("id")
+            logger.warning("pin_id: " + str(pin_id))
 
             get_response = test_client.get(f"/pins/{pin_id}").json()
 
