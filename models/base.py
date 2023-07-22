2eee3cc74b2b7e0b1bd3a1326ac35c3d6e23d6e6
Dan Filla <danfilla@gmail.com>
2023-07-22 15:32:26 -0500

    Added created_at and updated_at to base model

--- models/base.py
+++ models/base.py
@@ -1,7 +1,9 @@
-from sqlalchemy import Column, Integer
+from sqlalchemy import Column, Integer, DateTime, func
 
 from sqlalchemy.orm import DeclarativeBase
 
 
 class Base(DeclarativeBase):
     id = Column(Integer, primary_key=True)
+    created_at = Column(DateTime, default=func.now())
+    updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
